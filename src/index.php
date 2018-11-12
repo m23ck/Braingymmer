d4366e94a0b57ae438b5abf3ac1daf9e99d41179
m23ck <andojomack@gmail.com>
2018-11-12 19:58:21 -0300

    updated login and registrationscreen

--- src/index.php
+++ src/index.php
@@ -6,14 +6,40 @@
     <title>Braingymmer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <script type="text/javascript" src="main.js"></script>
 </head>
 <body>
-    
-    <div class="navigation">
 
-        <input type="button" name="btnLogin" value="Log in" onclick="Login()">
-        <input type="button" name="btnRegister" value="Register" onclick="Register()">
+
+<div class="navigation">
+<button type="button" name="openloginform" id="openloginform" onclick="document.getElementById('registrationform').style.display='none'; document.getElementById('loginform').style.display='block';">Login</button>
+<button type="button" name="openregistrationform" id="openregistrationform" onclick="document.getElementById('loginform').style.display='none'; document.getElementById('registrationform').style.display='block';">Register</button>
+</div>
+
+
+    <div class="container">
+        <form action="home.php" method="POST" id="loginform">
+            
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            <input type="password" name="password" placeholder="Password" >
+            <br>
+            <button type="submit">Login</button>
+        </form>
+
+        <form action="index.php" method="POST" style="display: none;" id="registrationform">
+           
+            <input type="text" name="username" placeholder="Username">
+            <br>
+            <input type="email" name="email" placeholder="Email">
+            <br>
+            <input type="password" name="password" placeholder="Password" >
+            <br>
+            <input type="password" name="retypePassword" placeholder="Retype Password">
+            <button type="submit">Register</button>
+        </form>
+        
+
 
     </div>
 
